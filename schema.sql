b84df01c798be41aa32dfa9e5c11a8caafa771db
yusungsamuel <ssyyu@ucdavis.edu>
2019-03-25 14:37:58 -0700

    write schema for making bamazon database

---
+++ schema.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS bamazon;
+
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products (
+    item_id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY(id),
+    product_name VARCHAR(100) NOT NULL,
+    department_name VARCHAR(50),
+    price DECIMAL(10, 2) NOT NULL,
+    stock_quantity INT NOT NULL
+)
